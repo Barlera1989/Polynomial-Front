75a02955a48b3a34464dd3b8b20844156ad9497a
rafael <barlera1989@gmail.com>
2021-03-21 18:43:26 -0300

    feature: working project development

---
+++ src/components/index.jsx
@@ -0,0 +1,182 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useFormik } from 'formik';
+
+
+import {
+  CenterContainer,
+  RowContainer,
+  ColumnContainer,
+  FormsContainer,
+  StyledText,
+  Button,
+  Label,
+  Input,
+  StyledSpanText
+} from './styles/styles'
+
+
+
+
+const MainPage = () => {
+  const [results, setResults] = useState(false)
+  const [requestedData, setRequestedData] = useState('')
+  /* const [triggerState, setTriggerState] = useState(false) */
+  const [warning, setWarning] = useState(false)
+
+
+  const environment = {
+    development: 'http://127.0.0.1:5000',
+    production: ''
+  }
+
+  /* useEffect(() => {
+
+  },// eslint-disable-next-line react-hooks/exhaustive-deps
+    [triggerState]) */
+
+
+  const getConstantsValues = useFormik({
+    initialValues: {
+      n_values: '',
+      k_value: '',
+      initial_value: '',
+      max_iterations: '',
+      tolerance: ''
+    },
+    onSubmit: values => {
+      setWarning(true)
+
+      setTimeout(setResults, 500, true)
+
+      let requestObject = { ...values, n_values: JSON.parse("[" + values.n_values + "]") }
+      sendZeroRequest(requestObject)
+
+      setTimeout(setWarning, 1000, false)
+
+    }
+
+  });
+
+
+
+  const sendZeroRequest = (data) => {
+
+    axios.post(`${environment.development}/request`,
+      { ...data })
+      .then((res) => {
+        console.log(res)
+        setRequestedData(res.data.Eq_zero)
+      })
+      .catch((res) => { })
+  }
+
+  const handleGraphics = () => {
+    axios.post(`${environment.development}/graphic`)
+      .then((res) => { })
+      .catch((res) => { })
+
+  }
+
+
+
+  return (
+    <CenterContainer>
+
+      <RowContainer height={50} align_items={'center'}>
+        <StyledText font_size={25}>Polynomial Calculator</StyledText>
+      </RowContainer>
+
+
+      <RowContainer width={350} height={50} align_items={'center'}>
+        <StyledText text_align={'left'} width={75}>Function:</StyledText>
+        <StyledText > f(x) = p(x) + k * cos(x) </StyledText>
+      </RowContainer>
+      <StyledText height={50} padding={10} font_size={10} text_align={'left'}>
+        Note: The power of the polynomial is quantity of items in the n values minus 1.
+        For example, the input is 4,3,2,1 have four items, so the power of polynomial is three.
+        Also, we have a k constant for cos. Putting k value: 3,
+        this example will generate this polynomial:4x^3 + 3x^2 + 2x^1 + 1x^0 + 3*cos(x). </StyledText>
+
+      <RowContainer >
+        <RowContainer width={300} height={220}>
+          <FormsContainer height={200} onSubmit={getConstantsValues.handleSubmit}>
+            <RowContainer>
+              <Label width={110} htmlFor="n_values">N values:</Label>
+              <Input
+                width={150}
+                id="n_values"
+                name="n_values"
+                type="n_values"
+                onChange={getConstantsValues.handleChange}
+                value={getConstantsValues.values.n_values}
+              />
+            </RowContainer>
+            <RowContainer>
+              <Label width={110} htmlFor="k_value">K value:</Label>
+              <Input
+                width={150}
+                id="k_value"
+                name="k_value"
+                type="k_value"
+                onChange={getConstantsValues.handleChange}
+                value={getConstantsValues.values.k_value}
+              />
+            </RowContainer>
+            <RowContainer>
+              <Label width={110} htmlFor="initial_value">Initial Value:</Label>
+              <Input
+                width={150}
+                id="initial_value"
+                name="initial_value"
+                type="initial_value"
+                onChange={getConstantsValues.handleChange}
+                value={getConstantsValues.values.initial_value}
+              />
+            </RowContainer>
+            <RowContainer>
+              <Label width={110} htmlFor="max_iterations">Max Iterations:</Label>
+              <Input
+                width={150}
+                id="max_iterations"
+                name="max_iterations"
+                type="max_iterations"
+                onChange={getConstantsValues.handleChange}
+                value={getConstantsValues.values.max_iterations}
+              />
+            </RowContainer>
+            <RowContainer>
+              <Label width={110} htmlFor="tolerance">Tolerance:</Label>
+              <Input
+                width={150}
+                id="tolerance"
+                name="tolerance"
+                type="tolerance"
+                onChange={getConstantsValues.handleChange}
+                value={getConstantsValues.values.tolerance}
+              />
+            </RowContainer>
+            <RowContainer align_items={'center'}>
+              <Button type="submit">Send!</Button>
+            </RowContainer>
+          </FormsContainer>
+        </RowContainer>
+      </RowContainer>
+
+      {results && <ColumnContainer width={300}>
+        <StyledText font_size={20} text_align={'left'}> Results:{warning && <StyledSpanText color={"#DD0000"} font_size={12}>...loading</StyledSpanText>}</StyledText>
+        <StyledText font_size={14} text_align={'left'}> Zero of equation: {requestedData ? requestedData : <span>Zero not Found!</span>}</StyledText>
+        <StyledText font_size={14} text_align={'left'}>Absolute Error: </StyledText>
+        <Button onClick={handleGraphics} >See Graphics</Button>
+      </ColumnContainer>}
+
+
+    </CenterContainer >
+
+  )
+}
+
+
+export default MainPage
+
+
---
+++ src/components/styles/styles.js
@@ -0,0 +1,80 @@
+import styled from 'styled-components'
+
+export const CenterContainer = styled.div`
+width: 400px;
+height:600px;
+display: flex;
+flex-direction:column;
+align-items: center;
+border-radius: 10px;
+border: 1px solid #CCC;
+background-image: linear-gradient(to bottom right, rgba(0, 0, 0, 0.05), #FFF);
+`
+
+export const RowContainer = styled.div`
+width: ${(props) => `${props.width}px`};
+height: ${(props) => `${props.height}px`};
+display: flex;
+flex-direction:row;
+align-items: ${(props) => `${props.align_items}`};
+`
+
+export const ColumnContainer = styled.div`
+width: ${(props) => `${props.width}px`};
+height: ${(props) => `${props.height}px`};
+display: flex;
+flex-direction:column;
+`
+
+export const FormsContainer = styled.form`
+height: ${(props) => `${props.height}px`};
+display:flex;
+flex-direction: row;
+flex-wrap: wrap;
+padding: 5px;
+`
+
+
+
+export const StyledText = styled.div`
+width: ${(props) => `${props.width}px`};
+padding: ${(props) => `${props.padding}px`};
+font-size: ${(props) => `${props.font_size}px`};
+color: ${(props) => `${props.color}`};
+text-align: ${(props) => `${props.text_align}`};
+position: relative;
+top: ${(props) => `${(props.top)}px`};
+`
+
+
+export const Button = styled.button`
+margin: 20px 5px;
+width: 200px;
+height: 30px;
+border-radius: 2px;
+font-size: 12px;
+color: #DDDDDD;
+background-color: #350060;
+`
+
+export const Label = styled.label`
+width: ${(props) => `${props.width}px`};
+display:flex;
+flex-direction: column;
+padding: 5px;
+text-align: left;
+`
+
+export const Input = styled.input`
+width: ${(props) => `${props.width}px`};
+height: 20px;
+border-radius: 4px;
+`
+
+export const StyledSpanText = styled.span`
+position: relative;
+left: 30px;
+font-size: ${(props) => `${props.font_size}px`};
+color: ${(props) => `${props.color}`};
+`
+
